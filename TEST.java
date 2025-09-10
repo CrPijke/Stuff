a49e9e6614df08be78d2108cf82265496d13143d
CrPijke <pijke@cruijff.be>
2025-09-10 16:36:27 +0200

    Calc

--- TEST.java
+++ TEST.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
-class ADD {
+class Calculator {
   public static void main(String[] args) {
 
-    // Request Input
+    // Request Method
+    Scanner operation = new Scanner(System.in);
+    String answer1;
+    System.out.println("Do you want to add or subtract?");
+    answer1 = operation.next();
+
+    //Request First number
+
     Scanner num1 = new Scanner(System.in);
     System.out.println("Enter first number");
 
     int Firstnum = num1.nextInt();
     System.out.println("First Number is : " + Firstnum);
 
+    //Request Second number
 
     Scanner num2 = new Scanner(System.in); 
     System.out.println("Enter second number");
@@ -21,11 +29,24 @@ class ADD {
     int x = Firstnum;
     int y = Secondnum;
 
-    // add two numbers
-    int sum = x + y;
-    System.out.println(x + "+" + y + "=" + sum);
+    if (answer1.equals("add")) {
+        int sum = addThings(x,y);
+        System.out.println(x + "+" + y + "=" + sum);
+    } else if (answer1.equals("subtract")) {
+        int subtract = subtractThings(x,y);
+        System.out.println(x + "-" + y + "=" + subtract);
+    }
+    
 
     num1.close();
     num2.close();
+    operation.close();
+  }
+  private static int addThings(int num1, int num2) {
+    return num1 + num2;
+  }
+
+  private static int subtractThings(int num1, int num2) {
+    return num1 - num2;
   }
 }
