a0451cbf3aa90f7c4b1ce98d9f0f415983c95dc1
CrPijke <pijke@cruijff.be>
2025-09-10 18:13:03 +0200

    Added more things

--- TEST.java
+++ TEST.java
@@ -6,7 +6,7 @@ class Calculator {
     // Request Method
     Scanner operation = new Scanner(System.in);
     String answer1;
-    System.out.println("Do you want to add or subtract?");
+    System.out.println("Do you want to add, subtract, multiply or divide?");
     answer1 = operation.next();
 
     //Request First number
@@ -35,12 +35,20 @@ class Calculator {
     } else if (answer1.equals("subtract")) {
         int subtract = subtractThings(x,y);
         System.out.println(x + "-" + y + "=" + subtract);
+    } else if (answer1.equals("multiply")) {
+      int multiply = multiplyThings(x, y);
+      System.out.println(x + "*" + y + "=" + multiply);
+    } else if (answer1.equals("divide")) {
+      int divide = divideThings(x, y);
+      System.out.println(x + "/" + y + "=" + divide);
     }
     
 
     num1.close();
     num2.close();
     operation.close();
+
+
   }
   private static int addThings(int num1, int num2) {
     return num1 + num2;
@@ -48,5 +56,13 @@ class Calculator {
 
   private static int subtractThings(int num1, int num2) {
     return num1 - num2;
+  }
+
+  private static int multiplyThings(int num1, int num2) {
+    return num1 * num2;
+  }
+
+  private static int divideThings(int num1, int num2) {
+    return num1 / num2;
   }
 }
